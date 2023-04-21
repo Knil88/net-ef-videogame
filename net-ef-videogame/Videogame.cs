09618ea1690b70bc4cd56f10b0616e155c75d594
Yari <narducciyarigmail.com>
2023-04-21 14:54:35 +0200

    creazioni classi e prova database

---
+++ net-ef-videogame/Videogame.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_ef_videogame
+{
+    public class Videogame
+    {
+        public long Id { get; set; }
+        public string name { get; set; }
+        public string overview { get; set; }
+        public string created_at { get; set; }
+        public string updated_at { get; set; }
+        public string software_house_id { get; set; }
+
+    }
